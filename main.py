86c8b81900e8a6ee44916eff85b23358fabf4bf5
Jacky <59948446+WJ2K@users.noreply.github.com>
2023-09-24 09:50:45 -0400

    Update main.py

--- main.py
+++ main.py
@@ -12,15 +12,9 @@ home = """
 # Welcome to Sensai
 
 ## Introduction
+At Sensai, we offer customized education experiences tailored to your unique needs, just as TikTok delivers personalized, bite-sized content. Powered by GPT-4, Sensai uses advanced prompt engineering to curate a learning environment where the AI adapts to you, not the other way around. But we go a step further—our intelligent feedback loop gathers insights from your past interactions, ensuring that each learning session is better than the last. We're on a mission to democratize education, making it accessible, inclusive, and engaging for everyone, whether you're a student, a professional, or a lifelong learner. In a world where education often takes a one-size-fits-all approach, Sensai is the game-changer you've been waiting for. 
 
-Sensai is revolutionizing the educational landscape by providing an intelligent, adaptive learning experience for students. In an age where one-size-fits-all models of education fall short, Sensai aims to bridge the gap between learning resources and individual needs using state-of-the-art machine learning technology.
-
-### Inspiration
-At Sensai, we draw inspiration from platforms like TikTok, where technology has the power to engage millions of users daily. Just as TikTok's recommendation algorithm customizes content to fit each user's preferences, we aim to bring The individualized approach of Sensai ensures that learning is targeted and efficient. Because it adapts to each user's needs, the system provides a highly engaging learning experience that conventional resources can't match. Furthermore, Sensai's capability to learn from past interactions allows it to constantly improve and better serve the needs of each learner.
-Our Inspiration comes from the idea that learning should be as unique as each individual. Tiktok's success demonstrates the incredible potential for personalized technology experiences, and we're excited to bring that same level of excitement to the realm of education with Sensai.
-
-## How is Sensai going to create an adaptable learning experience for students?
-
+## How it Sensai going to create an accessible and inclusive learning experience?
 Sensai relies on two important concepts: Prompt Engineering and the ability for LLM to fine tune itself by relying on the retrieval and reflection of past experiences for improvement.<sup>[[1]](https://arxiv.org/abs/2309.04658)</sup>
 
 - **Prompt Engineering**: Sensai uses Large Language Models (LLMs), like ChatGPT, to interact with users. By leveraging the power of prompt engineering<sup>[[2]](https://help.openai.com/en/articles/6654000-best-practices-for-prompt-engineering-with-openai-api)</sup>, we tailor the system's responses to suit individual learning styles and requirements.<sup>[[3]](https://papers.ssrn.com/sol3/papers.cfm?abstract_id=4475995)</sup>
@@ -28,15 +22,15 @@ Sensai relies on two important concepts: Prompt Engineering and the ability for 
 - **Reflection on past experiences**: Sensai has a built-in feedback loop that allows it to gather insights from previous interactions. This not only enhances its future responses but also helps in offering a truly personalized learning journey.
 
 
-## Why It's Effective
+## Why It's Effective?
 
 Sensai's effectiveness lies in its seamless adaptation to each user's unique needs, delivering an engaging learning experience without the need for users to master prompt engineering. 
 You don't have to be an expert in crafting prompts or AI technology; Sensai simplifies the learning process for you, effortlessly tailoring content to match your learning style, much like having a personal tutor who understands your preferences. 
 This approach along side the efficient Feynman model for education<sup>[[4]](https://www.colorado.edu/artssciences-advising/resource-library/life-skills/the-feynman-technique-in-academic-coaching)</sup> not only makes learning effortless but also fuels continuous improvement. Sensai continues to learns from past interactions to become an even more valuable educational companion with each engagement.
 
-## Who Should Use This Resource
+## Who Should Use This Resource?
 
-Sensai is designed for students, educators, and anyone seeking to deepen their understanding of complex subjects. Whether you're looking for quick answers, extended study sessions, or a continuously adaptive learning partner, SenI is your go-to platform.
+Sensai is designed for students, educators, and anyone seeking to deepen their understanding of complex subjects. Whether you're looking for quick answers, extended study sessions, or a continuously adaptive learning partner, Sensai is your go-to platform.
 
 ## Get Started
 
